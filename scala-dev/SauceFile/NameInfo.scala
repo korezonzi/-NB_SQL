c91cacbec54a7fc3544b533c0854adc603b0a50a
Fuma Tsuchiya <31926749+korezonzi@users.noreply.github.com>
2018-12-21 13:08:25 +0900

    Add files via upload

---
+++ scala-dev/SauceFile/NameInfo.scala
@@ -0,0 +1,11 @@
+class NameInfo(val id: Long, val firstname: String, val lastname: String, val fullname:Fullname = new Fullname) {
+  def getFullname() = this.fullname.get(firstname,lastname)
+}
+
+class Fullname() {
+  def get(firstname: String, lastname: String) = firstname + " " + lastname
+}
+
+val nameinfo = new  NameInfo(1,"Fuma","Tsuchiya")
+println(nameinfo.getFullname())
+println(nameinfo)
