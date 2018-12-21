395b03c9df35eefef53cfe59abf5c986969acff3
Fuma Tsuchiya <31926749+korezonzi@users.noreply.github.com>
2018-12-21 13:09:07 +0900

    Delete pra_name.scala

--- scala-dev/SauceFile/pra_name.scala
+++
@@ -1,9 +0,0 @@
-class User(val id: Long, val firstname: String, val lastname: String, val fullname:Fullname = new Fullname) {
-  def getFullname() = this.fullname.get(firstname, lastname)
-}
-
-class Fullname() {
-  def get(firstname: String, lastname: String) = firstname + " " + lastname
-}
-val user1 = new User(1, "fuma", "tsuchiya")
-println(user1.getFullname())
